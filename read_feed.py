b9d1425636a9033759a2f92a89c8a6472504fa5c
suri <swigder@gmail.com>
2018-08-08 00:08:11 -0400

    cleanup, exception catching, and some todos to make code more stable

--- read_feed.py
+++ read_feed.py
@@ -3,10 +3,8 @@ from datetime import datetime
 
 from google.transit import gtfs_realtime_pb2
 
-# from twisted.internet import task
-# from twisted.internet import reactor
-
 import urllib.request
+
 
 with open('api.key', 'r') as f:
     api_key = f.read()
@@ -19,27 +17,24 @@ most_recent_trip = datetime.now()  # todo handle first update
 
 
 while True:
-    feed = gtfs_realtime_pb2.FeedMessage()
-    response = urllib.request.urlopen('http://datamine.mta.info/mta_esi.php?key={}&feed_id=16'.format(api_key))
-    feed.ParseFromString(response.read())
-    trips_in_feed = {}
-    for entity in feed.entity:
-        trip_update = entity.trip_update
-        trip = trip_update.trip
-        if trip.route_id == 'R' and trip.trip_id.endswith('N'):
-            for stop_time_update in trip_update.stop_time_update:
-                if stop_time_update.stop_id == r_77_northbound:
-                    trips_in_feed[trip.trip_id] = datetime.fromtimestamp(stop_time_update.arrival.time)
-    for key, value in pending_trips.items():
-        if key not in trips_in_feed:
-            print('{}: Northbound R train {} arrived at 77th St station, {} minutes after most recent train.'
-                  .format(value, key, (value-most_recent_trip).seconds / 60))
-            most_recent_trip = value  # todo technically a race condition although unlikely to be met
-    pending_trips = trips_in_feed
+    try:
+        feed = gtfs_realtime_pb2.FeedMessage()
+        response = urllib.request.urlopen('http://datamine.mta.info/mta_esi.php?key={}&feed_id=16'.format(api_key))
+        feed.ParseFromString(response.read())
+        trips_in_feed = {}
+        for entity in feed.entity:
+            trip_update = entity.trip_update
+            trip = trip_update.trip
+            if trip.route_id == 'R' and trip.trip_id.endswith('N'):
+                for stop_time_update in trip_update.stop_time_update:
+                    if stop_time_update.stop_id == r_77_northbound:
+                        trips_in_feed[trip.trip_id] = datetime.fromtimestamp(stop_time_update.arrival.time)
+        for key, value in pending_trips.items():
+            if key not in trips_in_feed:  # todo very rough method to estimate train arrival - verify actual timestamp
+                print('{}: Northbound R train {} arrived at 77th St station, {:.2f} minutes after most recent train.'
+                      .format(value, key, (value-most_recent_trip).seconds / 60))
+                most_recent_trip = value  # todo technically a race condition although unlikely to be met
+        pending_trips = trips_in_feed
+    except:  # don't care what happens, sleep and try again - todo maybe be a bit smarter
+        pass
     time.sleep(frequency)
-
-
-# read_feed_task = task.LoopingCall(read_feed)
-# read_feed_task.start(frequency)
-#
-# reactor.run()
